29de997dc98a2d38786205aec4d48dbf8f733c9a
Erik C. Forés <soc@erik.cat>
2025-05-02 17:54:28 +0200

    fix(nova): flushable error handler writer

--- nova/middleware/error_handler.go
+++ nova/middleware/error_handler.go
@@ -7,11 +7,24 @@ import (
 	"github.com/studiolambda/cosmos/nova"
 )
 
-// errorHandlerWriter is a small type designed to access the
+// rawErrorHandlerWriter is a small type designed to access the
 // response status code after it has been written.
-type errorHandlerWriter struct {
+type rawErrorHandlerWriter struct {
 	http.ResponseWriter
 	status int
+}
+
+// flushableErrorHandlerWriter is a small type that wraps the
+// [errorHandlerWriter] struct so that it supports the flush
+// operation.
+type flushableErrorHandlerWriter struct {
+	*rawErrorHandlerWriter
+	http.Flusher
+}
+
+type ErrorHandlerWriter interface {
+	http.ResponseWriter
+	Status() int
 }
 
 type ErrorHandlerDevHandler interface {
@@ -23,10 +36,30 @@ type ErrorHandlerOptions struct {
 	IsDev  bool
 }
 
+func (w *rawErrorHandlerWriter) Status() int {
+	return w.status
+}
+
 // WriteHeader overrides the [http.ResponseWriter].
-func (w *errorHandlerWriter) WriteHeader(status int) {
+func (w *rawErrorHandlerWriter) WriteHeader(status int) {
 	w.status = status
 	w.ResponseWriter.WriteHeader(status)
+}
+
+func wrapErrorHandlerWriter(w http.ResponseWriter) ErrorHandlerWriter {
+	sw := &rawErrorHandlerWriter{
+		ResponseWriter: w,
+		status:         http.StatusOK, // default status for when Write() is called.
+	}
+
+	if f, ok := w.(http.Flusher); ok {
+		return &flushableErrorHandlerWriter{
+			rawErrorHandlerWriter: sw,
+			Flusher:               f,
+		}
+	}
+
+	return sw
 }
 
 func ErrorHandler(options ErrorHandlerOptions) nova.Middleware {
@@ -38,19 +71,16 @@ func ErrorHandler(options ErrorHandlerOptions) nova.Middleware {
 
 	return func(next nova.Handler) nova.Handler {
 		return func(w http.ResponseWriter, r *http.Request) error {
-			sw := &errorHandlerWriter{
-				ResponseWriter: w,
-				status:         http.StatusOK, // default status for when Write() is called.
-			}
+			sw := wrapErrorHandlerWriter(w)
 
 			defer func() {
-				if sw.status >= 500 && sw.status < 600 {
+				if status := sw.Status(); status >= 500 && status < 600 {
 					options.Logger.ErrorContext(
 						r.Context(),
 						"request failed",
 						"method", r.Method,
 						"url", r.URL.String(),
-						"status", sw.status,
+						"status", status,
 					)
 				}
 			}()
